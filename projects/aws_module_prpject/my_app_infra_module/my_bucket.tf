1389c98f1c2dff8b5b4dc37d36ad57bbea38d893
Anju12345hub <127766118+Anju12345hub@users.noreply.github.com>
2023-07-31 11:56:37 +0530

    Update my_bucket.tf

--- projects/aws_module_prpject/my_app_infra_module/my_bucket.tf
+++ projects/aws_module_prpject/my_app_infra_module/my_bucket.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_" "my-app-bucket" {
+resource "aws_s3_bucket" "my-app-bucket" {
   bucket = "${var.my_env}-batch3-app-bucket"
   tags = {
     Name = "${var.my_env}-batch3-app-bucket"
